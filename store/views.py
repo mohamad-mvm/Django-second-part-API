ffeae4e08f64068e099a255c821bfcbcc1d0e2b1
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-03-23 18:31:51 +0430

    first API

--- store/views.py
+++ store/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    return Response('Hello, World!')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+    
 
 # Create your views here.
