a7b5806dc1b638bb51d50a6f0577aca9129f21eb
Mohammad Vahabian <mohamad.vahabian@gmail.com>
2022-03-31 20:27:48 +0430

    rest_framework

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('product_list/', views.product_list),
-    path('product_list/<int:id>', views.product_detail),
+    path('product_list/', views.ProductList.as_view()),
+    path('product_list/<int:id>', views.ProductDetail.as_view()),
     path('collection/', views.collection_list),
     path('collection/<int:id>', views.collection_detail),
 ]
--- store/views.py
+++ store/views.py
@@ -2,40 +2,80 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer,CollectionSerializer
 
-@api_view(['GET','POST'])
-def product_list(request):
-    if request.method == 'GET':
+
+
+class ProductList(APIView):
+    def get(self, request):
         products = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)  
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class ProductDetail(APIView):
+    def get_object(self, id):
+        return get_object_or_404(Product, pk=id)
 
-
-@api_view(['GET','PUT','DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+    def get(self, request, id):
+        product = self.get_object(id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, id):
+        product = self.get_object(id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        product = self.get_object(id)
         if product.orderitem_set.count() > 0:
             return Response({'error': 'Product can not be deleted because it is assosiate with an order item'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+# @api_view(['GET','POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#         products = Product.objects.select_related('collection').all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)  
+
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def product_detail(request, id):
+#     product = get_object_or_404(Product, pk=id)
+#     if request.method == 'GET':
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = ProductSerializer(product, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == 'DELETE':
+#         if product.orderitem_set.count() > 0:
+#             return Response({'error': 'Product can not be deleted because it is assosiate with an order item'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET','POST'])
